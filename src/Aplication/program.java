49bc9693407deeb6a8cbad0f1d2f5194aa93b689
Eric1557 <ericbbarbosa2019@gmail.com>
2024-07-22 11:07:34 -0300

    created position class

---
+++ src/Aplication/program.java
@@ -0,0 +1,14 @@
+package Aplication;
+
+import BoardGame.Position;
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		Position pos = new Position(3,5);
+		System.out.println(pos.ToString());
+
+	}
+
+}
