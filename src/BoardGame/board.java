dd4be840f4ac53b0258cfedc15ec9b2efc53d7a4
Eric1557 <ericbbarbosa2019@gmail.com>
2024-07-26 11:22:10 -0300

    BoardExceptions e programaçao defensiva

--- src/BoardGame/board.java
+++ src/BoardGame/board.java
@@ -7,6 +7,9 @@ public class Board {
 	private Piece[][] pieces;
 
 	public Board(int rows, int colunms) {
+		if (rows < 1 || colunms < 1) {
+			throw new BoardException("error creating board: there must be at least 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.colunms = colunms;
 		pieces = new Piece[rows][colunms];
@@ -16,29 +19,47 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColunms() {
 		return colunms;
 	}
 
-	public void setColunms(int colunms) {
-		this.colunms = colunms;
-	}
-
 	public Piece pieces(int row, int colunm) {
+		if (!PositionExists(row, colunm)) {
+			throw new BoardException("Position not on the board ");
+		}
 		return pieces[row][colunm];
 	}
 
-	public Piece pieces (Position position) {
-       return pieces [position.getRow()][position.getColunm()];
+	public Piece pieces(Position position) {
+		if (!PositionExists(position)) {
+			throw new BoardException("Position not on the board ");
+		}
+		return pieces[position.getRow()][position.getColunm()];
 	}
 
-	public void PlacePiece(Piece piece,Position position){
+	public void PlacePiece(Piece piece, Position position) {
+		if (ThereIsAPiece(position)) {
+			throw new BoardException("there is already a piece on position " + position);
+		}
 		pieces[position.getRow()][position.getColunm()] = piece;
 		piece.position = position;
 	}
-	
+
+	private Boolean PositionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < colunms;
+
+	}
+
+	public Boolean PositionExists(Position position) {
+		return PositionExists(position.getRow(), position.getColunm());
+
+	}
+
+	public Boolean ThereIsAPiece(Position position) {
+		if (!PositionExists(position)) {
+			throw new BoardException("Position not on the board ");
+		}
+		return pieces(position) != null;
+	}
+
 }
